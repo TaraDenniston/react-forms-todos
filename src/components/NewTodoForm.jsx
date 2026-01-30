fcafa493d05ad4b887dbd2dbe20d6b1b05b40167
TaraDenniston <taradenniston@gmail.com>
2026-01-30 13:10:57 -0500

    add form to form component

--- src/components/NewTodoForm.jsx
+++ src/components/NewTodoForm.jsx
@@ -2,12 +2,37 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
-const NewTodoForm = () => {
+const NewTodoForm = ({ addTodo }) => {
+  const INITIAL_STATE = {
+    todo: ''
+  };
+  const [formData, setFormData] = useState(INITIAL_STATE);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(formData => ({
+      ...formData,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo({ ...formData });
+    setFormData(INITIAL_STATE);
+  }
+
   return (
-    <div>
-      <p>add todo</p>
-      <button><FontAwesomeIcon icon={faCirclePlus} /></button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input 
+        id="todo"
+        type="text"
+        name="todo"
+        placeholder="Add new item"
+        value={formData.todo}
+        onChange={handleChange}
+      />
+      <button type="submit"><FontAwesomeIcon icon={faCirclePlus} /></button>
+    </form>
   )
 }
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 import NewTodoForm from './NewTodoForm';
+import { faUserInjured } from '@fortawesome/free-solid-svg-icons';
 
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
+  const addTodo = (newTodo) => {
+    setTodos(todos => [...todos, { ...newTodo, id: uuid(), completed: false }])
+  }
 
   return (
     <div>
       <h1>To Do</h1>
       <div>
-        <NewTodoForm />
+        <NewTodoForm addTodo={addTodo} />
       </div>
       <p>List of Todo Items</p>
     </div>
